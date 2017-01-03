96ef3fd35d8b7fe11201ea956e6cd1aa925e2776
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-03 11:40:00 -0700

    remove comment

--- app/router.js
+++ app/router.js
@@ -1,6 +1,5 @@
 import Ember from 'ember';
 import config from './config/environment';
-// import calendar from '/templates/calendar';
 
 const Router = Ember.Router.extend({
   location: config.locationType,
