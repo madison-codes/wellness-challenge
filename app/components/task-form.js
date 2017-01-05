997ca030780f1fc5083d11defd0123573ca64ee7
Adam Bernard <ab255@me.com>
2017-01-05 16:08:28 -0700

    Problem reading checkbox boolean

--- app/components/task-form.js
+++ app/components/task-form.js
@@ -8,22 +8,4 @@ export default Ember.Component.extend({
       this.sendAction('createAction');
     }
   }
-  // title: "",
-  //
-  // actions:{
-  //   submitTasks(model){
-  //       console.log('title', this.get('title'));
-  //   //   //Create the comment
-  //   //   const comment = this.store.createRecord('comment',{
-  //   //     comment: comment,
-  //   //     author: author
-  //   //   });
-  //   //   //Get our post
-  //   //   const post = this.controller.get('model');
-  //   //   //Add our comment to our existing post
-  //   //   post.get('comments').pushObject(comment);
-  //   //   //Save the child then the parent
-  //   //   comment.save().then(() => post.save());
-  //   }
-  // }
 });
