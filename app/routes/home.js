6561dcfeb9f0a32eb6a35bee171f68830965fa6f
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-05 23:16:52 -0700

    saving correct data to pouch db

--- app/routes/home.js
+++ app/routes/home.js
@@ -2,10 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
+  model(){
+    return this.store.findAll('task');
+  },
   actions: {
     submitTasks: function(tasks){
       console.log(tasks);
-      var updatedTasks = this.store.createRecord("task", tasks);
+      var updatedTasks = this.store.createRecord('task', tasks);
       updatedTasks.save();
       // this.set('sweat', true);
       // this.set('rest', true);
