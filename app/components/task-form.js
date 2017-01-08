b974751f8d13f0891b9341cbd59cc7531063fcb6
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-08 13:43:38 -0700

    render date correctly with accurate task info

--- app/components/task-form.js
+++ app/components/task-form.js
@@ -1,17 +1,18 @@
 import Ember from 'ember';
+import moment from 'moment';
 
 export default Ember.Component.extend({
   actions: {
     updateTasks: function() {
+      var date = moment(new Date()).format('ddd');
       var tasks = {
+        date: date,
         sweat: this.get('sweat') ? 1 : 0,
         rest: this.get('rest') ? 1 : 0,
         nutrition: this.get('nutrition') ? 1 : 0,
         personalWellness: this.get('personalWellness') ? 1 : 0
       };
-      console.log(tasks);
       this.sendAction('createAction', tasks);
-
       this.set('sweat', 0);
       this.set('rest', 0);
       this.set('nutrition', 0);
