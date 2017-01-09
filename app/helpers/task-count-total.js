202231d6feb55642bc8a97531d6544637deeb515
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-08 23:54:07 -0700

    sum per day

--- app/helpers/task-count-total.js
+++ app/helpers/task-count-total.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 let count = 0;
-export function taskCountTotal(params/*, hash*/) {
 
-  var sum = params.reduce((a, b) => {
+export function taskCountTotal(params) {
+
+  let sum = params.reduce((a, b) => {
     return a + b;
   });
   count =  count + sum;
@@ -11,20 +12,3 @@ export function taskCountTotal(params/*, hash*/) {
 }
 
 export default Ember.Helper.helper(taskCountTotal);
-//
-// App:
-//   User:
-//     Admin: true/ false
-//     Name:
-//     Data: ["date": {
-//       Rest: 1
-//       Sweat: 0
-//
-//     }]
-//
-//     Data: {
-//       Rest: [34523465, 24563456245, 245634574567, 24573457, 34573452456],
-//
-//     }
-//
-//     data.rest.includes(2324624573457);
