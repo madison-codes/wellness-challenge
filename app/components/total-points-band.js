953d0fa398ff92255baf4de76417215159020ef7
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-09 22:09:38 -0700

    broken sum points component

--- app/components/total-points-band.js
+++ app/components/total-points-band.js
@@ -1,12 +1,33 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  // model(){
-  //   return this.route.get('tasks');
-  // },
+  model(){
+    return this.route.get('model');
+  },
   actions: {
-    updateTasks: function(tasks){
-      return this.route.get('tasks');
+  sum(){
+    const rest = Ember.computed.sum('model.rest');
+    const sweat = Ember.computed.sum('model.sweat');
+
+    return rest + sweat;
+    // sum(model) {
+    //   let count = 0;
+    //   for(var i = 0; i < model.length; i++){
+    //     let sum = model.reduce((a, b) => {
+    //       return a + b;
+    //     });
+    //     count =  count + sum;
+    //   }
+    //   return count;
     }
   }
 });
+//
+// export function taskCountTotal(model) {
+//   let count = 0;
+//   let sum = params.reduce((a, b) => {
+//     return a + b;
+//   });
+//   count =  count + sum;
+//   return count;
+// }
