f8b45765b420a8daaea01f0a957a51464ccb76fe
mjvalade <mjvalade@gmail.com>
2017-01-09 22:20:53 -0700

    pseudo code acceptance tests

--- tests/acceptance/wellness-test.js
+++ tests/acceptance/wellness-test.js
@@ -13,5 +13,23 @@ test('visiting /login', function(assert) {
 
 
 skip('clicking on checkboxes and "submit" button registers daily tasks to database', function(assert) {
+// click on check boxes
+// click on submit button
+// how to check something is in the db?
+});
 
+skip('visit user dashboard page will render tasks of the week', function(assert) {
+  visit('/dashboard');
+// see {{task-count-day}} component
+// see {{task.date}} component
+// if tasks, render corresponding stars
+});
+
+skip('visit admin-dashboard page will render top ten users', function(assert) {
+  visit('/home');
+  // {{#if (eq session.data.authenticated.profile.name 'MaryJane ')}}
+// ^^ set admin auth to match
+// render {{admin-dashboard}} component
+// make and render {{top-ten-wk}} component
+// make and render {{top-ten-mod}} component
 });
