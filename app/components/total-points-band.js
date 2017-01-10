9a1b646b4167b427588ee51843d00d334275215c
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-09 18:39:54 -0700

    dashboard functionality

--- app/components/total-points-band.js
+++ app/components/total-points-band.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
 
-  updateTasks: function (tasks) {
-    // this.route.get('tasks');
-    // this.controller.get('tasks');
-    return tasks;
-  },
+  // updateTasks: function (tasks) {
+  //   // this.route.get('tasks');
+  //   // this.controller.get('tasks');
+  //   return tasks;
+  // },
 
 });
--- app/routes/dashboard.js
+++ app/routes/dashboard.js
@@ -1,12 +1,14 @@
 import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model(){
-    return this.store.findAll('task');
-  },
-  tasksCount: () {
-    return model.tasks.length;
-  }
-});
+export default Ember.Route.extend(AuthenticatedRouteMixin);
+  // model(){
+  //   return this.store.findAll('task');
+  // },
+  // actions: {
+    // taskCount: function(tasks){
+    //   return tasks;
+    // }
+    // add action to search specific dates to render results for week or mod
+  // }
+// });
