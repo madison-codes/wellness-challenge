a9d720853a942fc5a4f6414285d005e40b701a92
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-11 08:06:22 -0700

    find individual values for each activity

--- app/components/dashboard-graph-one.js
+++ app/components/dashboard-graph-one.js
@@ -2,5 +2,16 @@ import Ember from 'ember';
 // import VerticalBarChartComponent from 'ember-charts/components/vertical-bar-chart';
 
 export default Ember.Component.extend({
-  // store: this.routes.get('tasks')
+  sweat: Ember.computed.filterBy('tasks','sweat', 1),
+  rest: Ember.computed.filterBy('tasks','rest', 1),
+  nutrition: Ember.computed.filterBy('tasks','nutrition', 1),
+  personalWellness: Ember.computed.filterBy('tasks','personalWellness', 1),
+
+  actions: {
+    graphData(model) {
+      const data = JSON.parse((model));
+      console.log(data);
+      // return data;
+    }
+  }
 });
