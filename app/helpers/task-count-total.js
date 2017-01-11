29b00356af7c495b4fe0ba8928a21eec6604f26e
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-11 13:45:27 -0700

    code clean up

--- app/helpers/task-count-total.js
+++ app/helpers/task-count-total.js
@@ -1,14 +1,11 @@
 import Ember from 'ember';
 
 let count = 0;
-
 export function taskCountTotal(params) {
-
   let sum = params.reduce((a, b) => {
     return a + b;
   });
   count =  count + sum;
   return count;
 }
-
 export default Ember.Helper.helper(taskCountTotal);
--- config/environment.js
+++ config/environment.js
@@ -10,24 +10,15 @@ module.exports = function(environment) {
 
     EmberENV: {
       FEATURES: {
-      // Here you can enable experimental features on an ember canary build
-      // e.g. 'with-controller': true
     }
   },
 
   APP: {
-    // Here you can pass flags/options to your application instance
-    // when it is created
   }
 };
 
   if (environment === 'development') {
 
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
 
     ENV['ember-simple-auth'] = {
       authenticationRoute: 'index',
@@ -42,10 +33,8 @@ module.exports = function(environment) {
   }
 
   if (environment === 'test') {
-    // Testem prefers this...
     ENV.locationType = 'none';
 
-    // keep test console output quieter
     ENV.APP.LOG_ACTIVE_GENERATION = false;
     ENV.APP.LOG_VIEW_LOOKUPS = false;
 
