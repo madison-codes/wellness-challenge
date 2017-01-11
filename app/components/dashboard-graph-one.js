f35adcee2c0235a2ded233585a2fc686f9192083
Madison Kerndt <madison.kerndt@gmail.com>
2017-01-11 11:14:51 -0700

    graph

--- app/components/dashboard-graph-one.js
+++ app/components/dashboard-graph-one.js
@@ -2,16 +2,10 @@ import Ember from 'ember';
 // import VerticalBarChartComponent from 'ember-charts/components/vertical-bar-chart';
 
 export default Ember.Component.extend({
-  sweat: Ember.computed.filterBy('tasks','sweat', 1),
-  rest: Ember.computed.filterBy('tasks','rest', 1),
-  nutrition: Ember.computed.filterBy('tasks','nutrition', 1),
-  personalWellness: Ember.computed.filterBy('tasks','personalWellness', 1),
-
-  actions: {
-    graphData(model) {
-      const data = JSON.parse((model));
-      console.log(data);
-      // return data;
-    }
-  }
+  // tasks: model,
+  // height:
+  rest: Ember.computed('tasks.@each.rest', function() {
+    var tasks = this.get('tasks');
+    return tasks.filterBy('rest', true);
+  })
 });
